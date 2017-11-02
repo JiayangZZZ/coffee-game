e54f507968f4db370c52b40df69b979b47e0c3ac
Jiayang Zhang <jiayangzhang1104@gmail.com>
2017-11-01 21:33:52 -0700

    Two buttons control two sets of data. Update images with smaller-sized

--- public/js/game.js
+++ public/js/game.js
@@ -1,38 +1,119 @@
 
 var renderer = PIXI.autoDetectRenderer(800, 600, {
 	antialias: false,
-	transparent: false,
+	transparent: true,
 	resolution: 1
 });
 
 document.getElementById('display').appendChild(renderer.view);
 
+//varables
+var caffeine = 0;
+var metaRate = 9;
+
+//PIXI containers
 var stage = new PIXI.Container();
+var controller = new PIXI.Container();
+
+
 
 PIXI.loader
-	.add('/images/1.png')
+	.add('dying', '/images/1.png')
+	.add('shot', '/images/shot.png')
+	.add('water', '/images/water.png')
 	.load(setup);
 
-var hero;
+var hero,
+	buttonShot,
+	buttonWater;
 
 function setup() {
-	hero = new PIXI.Sprite(
-		PIXI.loader.resources['/images/1.png'].texture
+	controller.interactive = true;
+	// hero = new PIXI.Sprite(
+	// 	PIXI.loader.resources['dying'].texture
+	// );
+
+	// hero.scale.set(0.3, 0.3);
+
+	buttonShot = new PIXI.Sprite(
+		PIXI.loader.resources['shot'].texture
+	);
+	buttonWater = new PIXI.Sprite(
+		PIXI.loader.resources['water'].texture
 	);
 
-	hero.scale.set(0.3, 0.3);
+	buttonShot.scale.set(0.4, 0.4);
+	// buttonShot.anchor.set(0.5, 0.5);
+	buttonWater.scale.set(0.4, 0.4);
+	// buttonWater.anchor.set(0.5, 0.5);
+	buttonWater.y = renderer.height - buttonWater.height;
 
-	stage.addChild(hero);
+	buttonShot.click = function() {
+		caffeine += 77;
+		document.getElementById('caffeine').innerHTML = caffeine;
+	}
+	buttonShot.mouseover = function() {
+		buttonShot.width += 5;
+		buttonShot.height += 5;
+	}
+	buttonShot.mouseout = function() {
+		buttonShot.width -= 5;
+		buttonShot.height -= 5;
+	}
+
+	buttonWater.click = function() {
+		metaRate += 1;
+		document.getElementById('metaRate').innerHTML = metaRate;
+	}
+	buttonWater.mouseover = function() {
+		buttonWater.width += 5;
+		buttonWater.height += 5;
+	}
+	buttonWater.mouseout = function() {
+		buttonWater.width -= 5;
+		buttonWater.height -= 5;
+	}
+
+	buttonShot.interactive = true;
+	buttonShot.buttonMode = true;
+	buttonWater.interactive = true;
+	buttonWater.buttonMode = true;
+	controller.addChild(buttonShot);
+	controller.addChild(buttonWater);
+
+	// stage.addChild(hero);
+	animationLoop();
+}
+
+function animationLoop() {
+	requestAnimationFrame(animationLoop);
+
+	
+	
+
 	renderer.render(stage);
+	renderer.render(controller);
 }
 
 function setTime() {
 	var timer = document.getElementById('timer').innerHTML;
 	var ampm = document.getElementById('ampm').innerHTML;
+	var rate = document.getElementById('metaRate').innerHTML;
+	var caffeine = document.getElementById('caffeine').innerHTML;
 
 	var h = parseInt(timer);
+	caffeine = parseInt(caffeine);
+	rate = parseInt(rate);
+
 	h++;
 	document.getElementById('timer').innerHTML = "" + h + "";
+	if(caffeine >= rate) {
+		document.getElementById('caffeine').innerHTML = "" + (caffeine-rate) + "";
+	}
+	else {
+		document.getElementById('caffeine').innerHTML = 0;
+	}
+	
 	if(h > 12) {
 		document.getElementById('timer').innerHTML = 1;
 		if(ampm == 'AM') {
